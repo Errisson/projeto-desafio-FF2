860da75dc6b16e5d9244ed40792203e144699fa3
Errissonds <errissonds16@hotmail.com>
2024-10-19 15:05:32 -0300

    primeira fase

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Começando
 let name = ("")
+
+let quantidadeVitorias = 1
+let quantidadeDerrotas = 2
+
+let saldoDeVitorias = ( quantidadeVitorias - quantidadeDerrotas )
+console.log ( saldoDeVitorias )
+
+if  (saldoDeVitorias < 10 ) 
+    {
+    console.log ( "O Herói tem de saldo " + saldoDeVitorias + " está no nível de Ferro!")
+}
